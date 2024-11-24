1875c294f3147211211bebc12f84c58da6c8127c
Schaechtle <u.schaechtle@gmail.com>
2024-11-24 16:53:28 -0500

    feat: Plot choropleth map of Japan

--- src/lpm_plot/__init__.py
+++ src/lpm_plot/__init__.py
@@ -4,3 +4,4 @@ from .plot_fidelity import plot_fidelity
 from .plot_marginal import plot_marginal_1d
 from .plot_marginal import plot_marginal_2d
 from .plot_heatmap import plot_heatmap
+from .plot_maps import plot_map
