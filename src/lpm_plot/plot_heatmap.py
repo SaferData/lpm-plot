77e884491832dc2c74694332d795cde7fccc2430
npovejsil <npovejsil@berkeley.edu>
2025-10-02 17:10:18 -0400

    chore: move ruff to dev dependencies, remove black

--- src/lpm_plot/plot_heatmap.py
+++ src/lpm_plot/plot_heatmap.py
@@ -115,9 +115,7 @@ def plot_heatmap(
             ),
         ),
         tooltip=["Column 1", "Column 2", "Score:Q"],
-    ).properties(
-        width=400, height=400
-    )  # Default scale
+    ).properties(width=400, height=400)  # Default scale
 
     # Return heatmap if no detailed data is provided
     if detailed_df is None:
